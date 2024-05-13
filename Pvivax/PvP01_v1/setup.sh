d1fe6092cce3c79e7869d38dc91e21ca87bf8374
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-05-13 15:10:23 +0930

    fix: install script now use refs/ inside configs/

--- Pvivax/PvP01_v1/setup.sh
+++ Pvivax/PvP01_v1/setup.sh
@@ -11,27 +11,28 @@ cd ${NGSENV_BASEDIR}
 LABEL=PvP01_v1
 REF=PlasmoDB-50_PvivaxP01
 HOSTREF=GCF_000001405.40_GRCh38.p14
+REFDIR=configs/refs/${LABEL}
 (	
 	echo "Downloading ${REF} from PlasmoDB..."
-	mkdir refs/${LABEL}
-	cd refs/${LABEL}
+	mkdir -p ${REFDIR}
+	cd ${REFDIR}
 	curl -z ${HOSTREF}_genomic.fna -O https://plasmodb.org/common/downloads/release-50/PvivaxP01/fasta/data/${REF}_Genome.fasta
 )
 
 (
 	echo "Downloading host ${HOSTREF} from NCBI FTP site..."
-	cd refs/${LABEL}
+	cd ${REFDIR}
     curl -z ${HOSTREF}_genomic.fna -O https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/latest_assembly_versions/${HOSTREF}/${HOSTREF}_genomic.fna.gz
     echo "Decompressing host reference sequences..."
     gunzip -fk ${HOSTREF}_genomic.fna.gz
     echo "Generating YAML region file..."
-    ngs-pl setup-references -n -k contaminant_regions -o contaminants.yaml -f ${HOSTREF}_genomic.fna
+    ngs-pl setup-references -n -k contaminant_regions -o contaminants.yaml -f ${HOSTREF}_genomic.fna --pattern "NC_.*" --outfasta contaminants.fasta
 )
 
 (
     echo "Concatenating parasite and host sequences..."
-    cd refs/${LABEL}
-    cat ${REF}_Genome.fasta ${HOSTREF}_genomic.fna > ${LABEL}-GRCh38.p14.fasta
+    cd ${REFDIR}
+    cat ${REF}_Genome.fasta contaminants.fasta > ${LABEL}-GRCh38.p14.fasta
 )
 
 (
@@ -40,21 +41,21 @@ HOSTREF=GCF_000001405.40_GRCh38.p14
 	curl -O https://raw.githubusercontent.com/vivaxgen/vgnpc-plasmodium-spp/main/Pvivax/${LABEL}/${LABEL}-all.yaml
 
 	echo "Appending contaminant regions..."
-	cat ../refs/${LABEL}/contaminants.yaml >> ${LABEL}-all.yaml
+	cat refs/${LABEL}/contaminants.yaml >> ${LABEL}-all.yaml
 )
 
 (
 	echo "Downloading and preparing known variant from Pv4 dataset..."
-	cd refs/${LABEL}
+	cd ${REFDIR}
 	curl -O https://raw.githubusercontent.com/vivaxgen/vgnpc-plasmodium-spp/main/Pvivax/${LABEL}/known-variants.bed.gz
 	mkdir known-variants
-	python3 -c "import yaml; [open(f'known-variants/{reg}.bed', 'w') for reg in yaml.safe_load(open('../../configs/${LABEL}-all.yaml'))['regions']]"
+	python3 -c "import yaml; [open(f'known-variants/{reg}.bed', 'w') for reg in yaml.safe_load(open('../../${LABEL}-all.yaml'))['regions']]"
 	zcat known-variants.bed.gz | awk '{print > "known-variants/" $1 ".bed"}'
 	for fn in known-variants/*.bed; do echo "Processing ${fn}"; bgzip -f ${fn}; tabix ${fn}.gz; done
 )
 
 echo "Linking config.yaml..."
-ln -srf configs/${LABEL}-all.yaml config.yaml
+ln -srf configs/${LABEL}-all.yaml configs/config.yaml
 
 echo "Indexing reference sequence"
 ngs-pl initialize --target wgs
