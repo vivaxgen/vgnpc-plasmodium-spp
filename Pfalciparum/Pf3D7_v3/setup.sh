f59b66db5f628b996d71d818bc8d20cf7290a101
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-10-18 09:39:04 +0930

    fix: setup.sh for Pf3D7_v2

--- Pfalciparum/Pf3D7_v3/setup.sh
+++ Pfalciparum/Pf3D7_v3/setup.sh
@@ -9,29 +9,37 @@ echo "Entering directory: ${NGSENV_BASEDIR}"
 cd ${NGSENV_BASEDIR}
 
 LABEL=Pf3D7_v3
-REF=PlasmoDB-67_Pfalciparum3D7
+REF=PlasmoDB-68_Pfalciparum3D7
 HOSTREF=GCF_000001405.40_GRCh38.p14
+REFDIR=configs/refs/${LABEL}
 (	
 	echo "Downloading ${REF} from PlasmoDB..."
-	mkdir refs/${LABEL}
-	cd refs/${LABEL}
-	curl -z ${HOSTREF}_genomic.fna -O https://plasmodb.org/common/downloads/release-67/Pfalciparum3D7/fasta/data/${REF}_Genome.fasta
+	mkdir -p ${REFDIR}
+	cd ${REFDIR}
+	curl -z ${REF}_Genome.fasta        -O https://plasmodb.org/common/downloads/release-68/Pfalciparum3D7/fasta/data/${REF}_Genome.fasta
+	curl -z ${REF}_AnnotatedCDSs.fasta -O https://plasmodb.org/common/downloads/release-68/Pfalciparum3D7/fasta/data/${REF}_AnnotatedCDSs.fasta
+	curl -z ${REF}.gff                 -O https://plasmodb.org/common/downloads/release-68/Pfalciparum3D7/gff/data/${REF}.gff
+	ln -sr ${REF}_Genome.fasta ${LABEL}.fasta
+	ln -sr ${REF}.gff ${LABEL}.gff
+	ln -sr ${REF}_AnnotatedCDSs.fasta ${LABEL}_AnnotatedCDSs.fasta
 )
 
 (
 	echo "Downloading host ${HOSTREF} from NCBI FTP site..."
-	cd refs/${LABEL}
-    curl -z ${HOSTREF}_genomic.fna -O https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/latest_assembly_versions/${HOSTREF}/${HOSTREF}_genomic.fna.gz
-    echo "Decompressing host reference sequences..."
-    gunzip -fk ${HOSTREF}_genomic.fna.gz
-    echo "Generating YAML region file..."
-    ngs-pl setup-references -n -k contaminant_regions -o contaminants.yaml -f ${HOSTREF}_genomic.fna
+	cd ${REFDIR}
+	curl -z ${HOSTREF}_genomic.fna -O https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/latest_assembly_versions/${HOSTREF}/${HOSTREF}_genomic.fna.gz
+	echo "Decompressing host reference sequences..."
+	gunzip -fk ${HOSTREF}_genomic.fna.gz
+	echo "Generating YAML region file..."
+	ngs-pl setup-references -n -k contaminant_regions -o contaminants.yaml -f ${HOSTREF}_genomic.fna --pattern "NC_.*" --outfasta contaminants.fasta
+	rm ${HOSTREF}_genomic.fna
 )
 
 (
-    echo "Concatenating parasite and host sequences..."
-    cd refs/${LABEL}
-    cat ${REF}_Genome.fasta ${HOSTREF}_genomic.fna > ${LABEL}-GRCh38.p14.fasta
+	echo "Concatenating parasite and host sequences..."
+	cd ${REFDIR}
+	cat ${REF}_Genome.fasta contaminants.fasta > ${LABEL}-GRCh38.p14.fasta
+	rm contaminants.fasta
 )
 
 (
@@ -40,21 +48,21 @@ HOSTREF=GCF_000001405.40_GRCh38.p14
 	curl -O https://raw.githubusercontent.com/vivaxgen/vgnpc-plasmodium-spp/main/Pfalciparum/${LABEL}/${LABEL}-all.yaml
 
 	echo "Appending contaminant regions..."
-	cat ../refs/${LABEL}/contaminants.yaml >> ${LABEL}-all.yaml
+	cat refs/${LABEL}/contaminants.yaml >> ${LABEL}-all.yaml
 )
 
 (
-	echo "Downloading and preparing known variant from Pv4 dataset..."
-	cd refs/${LABEL}
+	echo "Downloading and preparing known variant from Pf7 dataset..."
+	cd ${REFDIR}
 	curl -O https://raw.githubusercontent.com/vivaxgen/vgnpc-plasmodium-spp/main/Pfalciparum/${LABEL}/known-variants.bed.gz
 	mkdir known-variants
-	python3 -c "import yaml; [open(f'known-variants/{reg}.bed', 'w') for reg in yaml.safe_load(open('../../configs/${LABEL}-all.yaml'))['regions']]"
+	python3 -c "import yaml; [open(f'known-variants/{reg}.bed', 'w') for reg in yaml.safe_load(open('../../${LABEL}-all.yaml'))['regions']]"
 	zcat known-variants.bed.gz | awk '{print > "known-variants/" $1 ".bed"}'
 	for fn in known-variants/*.bed; do echo "Processing ${fn}"; bgzip -f ${fn}; tabix ${fn}.gz; done
 )
 
 echo "Linking config.yaml..."
-ln -srf configs/${LABEL}-all.yaml config.yaml
+ln -srf configs/${LABEL}-all.yaml configs/config.yaml
 
 echo "Indexing reference sequence"
 ngs-pl initialize --target wgs
